f1cec3397e3c39bbef00dd6f8d4be59d2ea605a8
Ian Yang <me@iany.me>
2014-04-11 11:30:30 +0800

    Coerce undefined to undefined for pjm model.

--- src/pjm_parse_trans.erl
+++ src/pjm_parse_trans.erl
@@ -357,7 +357,8 @@ generate_coerce(#state{ module = Module }) ->
     [
      codegen:gen_function(
        coerce,
-       fun({'$var', Module}, {{'$var', Module}, _, _} = Model) -> Model;
+       fun({'$var', Module}, undefined) -> undefined;
+          ({'$var', Module}, {{'$var', Module}, _, _} = Model) -> Model;
           ({'$var', Module}, Attrs) when is_list(Attrs) -> new(Attrs);
           ({'$var', Module}, {Attrs}) when is_list(Attrs) -> new(Attrs)
        end
